01b80d65fce93062c6339354a0f4ae6f79722694
zhengmin <zouzhengmin@gmail.com>
2018-06-29 11:19:37 +0800

    change root path and add match to, no json

---
+++ app/controllers/fruits_controller.rb
@@ -0,0 +1,10 @@
+class FruitsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+  resources :fruits do
+    collection do
+      get :list
+      match 'tastegood', to: 'fruits#show', via: :all
+    end
+  end
+
+
+
+  root 'fruits#list'
+
+
+ end
